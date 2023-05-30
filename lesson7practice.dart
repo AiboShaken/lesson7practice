42b2d82b9c373f05b5a7f24bf3516477a1f8c3a3
AiboShaken <aiboshaken@gmail.com>
2023-05-30 16:38:09 +0600

    first commit

---
+++ lesson7practice.dart
@@ -0,0 +1,49 @@
+void main() {
+  // for (int i = 1; i <= 100; i++) {
+  //   print(i);
+  // }
+
+  // int i = 1;
+  // while (i <= 100) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // for (int i = 11; i <= 33; i++) {
+  //   print(i);
+  // }
+
+  // int i = 11;
+  // while (i <= 33) {
+  //   print(i);
+  //   i++;
+  // }
+
+  // for (int i = 0; i <= 100; i++) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+  // }
+
+  // int i = 1;
+  // while (i <= 100) {
+  //   if (i.isEven) {
+  //     print(i);
+  //   }
+  //   i++;
+  // }
+
+  // for (int i = 2; i <= 99; i++) {
+  //   print(i);
+  // }
+
+  // int i = 2;
+  // while (i <= 99) {
+  //   print(i);
+  //   i++;
+  // }
+
+  
+
+
+}
